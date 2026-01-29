db50c006eec31b6dea133ef7a2de567c335a2756
emmaculate-exceel <emmaculate2exceel@live.com>
2026-01-29 19:03:35 +0100

    added constructor to the classes

--- OOP/Customer.java
+++ OOP/Customer.java
@@ -1,4 +1,43 @@
 public class Customer{
+	// (Attributes)
+	String Manufacturer;
+	String Title;
+	String ProductNumber;
+	String Author;
+	String Performer;
+	String Director;
+
+	// (Constructor)
+
+	public static Book(String Manufacturer, String Title, String ProductNumber, String Author) {
+		this.Manufacturer;
+		this.Title;
+		this.ProductNumber;
+		this.Author;
+	}
+
+	public static Music(String Manufacturer, String Title, String ProductNumber, String Performer){
+		this.Manufacturer;
+		this.Title;
+		this.ProductNumber;
+		this.Performer;
+	}
+
+	public static Game(String Manufacturer, String Title, String ProductNumber, String Author) {
+		this.Manufacturer;
+		this.Title;
+		this.ProductNumber;
+		this.Performer;
+	}
+
+	public static Film(String Manufacturer, String Title, String ProductNumber, String Director) {
+		this.Manufacturer;
+		this.Title;
+		this.ProductNumber;
+		this.Director;
+	}
+
 	public void main(String[] args) {
+		System.out.println();
 	}
 }
